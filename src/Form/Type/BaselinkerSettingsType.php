a87fe5a3cbeea01f86dc90277aba4dc16d6c5992
Kamil Matulewicz <kamil.matulewicz@gmail.com>
2023-03-21 20:49:21 +0100

    fill setting from Baselinker API test

--- src/Form/Type/BaselinkerSettingsType.php
+++ src/Form/Type/BaselinkerSettingsType.php
@@ -6,17 +6,36 @@ namespace SyliusBaselinkerPlugin\Form\Type;
 
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
 use SyliusBaselinkerPlugin\Entity\BaselinkerSettings;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use SyliusBaselinkerPlugin\Services\BaselinkerOrdersApiService;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class BaselinkerSettingsType extends AbstractResourceType
 {
+    private BaselinkerOrdersApiService $baselinkerOrder;
+
+    /** @param  array<array-key, string> $validationGroups*/
+    public function __construct(string $dataClass, BaselinkerOrdersApiService $baselinkerOrder, array $validationGroups = [])
+    {
+        $this->baselinkerOrder = $baselinkerOrder;
+
+        parent::__construct($dataClass, $validationGroups);
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         /** @var BaselinkerSettings $setting */
         $setting = $options['data'];
 
-        $builder->add('value', TextType::class, ['label' => $setting->getName()]);
+        $sources = $this->baselinkerOrder->getOrderSources();
+
+        $flippedSources = [];
+        /** @var array<array-key, string> $sourceCategory */
+        foreach ($sources as $key => $sourceCategory) {
+            $flippedSources[$key] = array_flip($sourceCategory);
+        }
+
+        $builder->add('value', ChoiceType::class, ['label' => $setting->getName(), 'choices' => $flippedSources]);
     }
 
     public function getBlockPrefix()
