85967b748e9e78b73fc7a7f3ecb21edf44142677
Kamil Matulewicz <kamil.matulewicz@gmail.com>
2023-04-01 17:51:40 +0200

    SettingsType

---
+++ src/Grid/FieldType/SettingsType.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SyliusBaselinkerPlugin\Grid\FieldType;
+
+use Sylius\Component\Grid\Definition\Field;
+use Sylius\Component\Grid\FieldTypes\FieldTypeInterface;
+use SyliusBaselinkerPlugin\Entity\Settings;
+use SyliusBaselinkerPlugin\Service\OrdersApiService;
+
+class SettingsType implements FieldTypeInterface
+{
+    private array $sources = [];
+
+    public function __construct(OrdersApiService $ordersApi)
+    {
+        $rawSources = $ordersApi->getOrderSources();
+        foreach ($rawSources as $value) {
+            $this->sources = $this->sources + $value;
+        };
+    }
+
+    public function render(Field $field, $data, array $options = []): string
+    {
+        if (!($data instanceof Settings)) {
+            return '';
+        }
+
+        if ($data->getName() === 'order.source') {
+            $r = $value = $data->getValue();
+            $r .= (array_key_exists($value, $this->sources) ? ' (' . (string) $this->sources[$value] . ')' : '');
+
+            return htmlspecialchars($r);
+        }
+        return htmlspecialchars($data->getValue());
+    }
+
+    public function configureOptions(mixed $resolver): void
+    {
+    }
+
+    public function getName(): string
+    {
+        return 'status_connected';
+    }
+}
