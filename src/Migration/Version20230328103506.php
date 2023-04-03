278402ab6c3c3f221b7c0e65a19812bf9710f340
Kamil Matulewicz <kamil.matulewicz@gmail.com>
2023-04-03 12:52:08 +0200

    Update migration

--- src/Migration/Version20230328103506.php
+++ src/Migration/Version20230328103506.php
@@ -21,13 +21,13 @@ final class Version20230328103506 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE baselinker_settings (name VARCHAR(255) NOT NULL, value VARCHAR(255) NOT NULL, PRIMARY KEY(name)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE baselinker_statuses_associations (shopStatus VARCHAR(255) NOT NULL, baselinkerStatus VARCHAR(255) NOT NULL, PRIMARY KEY(shopStatus)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE sylius_order ADD baselinkerId INT DEFAULT 0 NOT NULL, ADD baselinkerUpdateTime INT DEFAULT 0 NOT NULL');
+        $this->addSql('CREATE TABLE baselinker_statuses_associations (shop_status VARCHAR(255) NOT NULL, baselinker_status VARCHAR(255) NOT NULL, PRIMARY KEY(shop_status)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE sylius_order ADD baselinker_id INT DEFAULT 0 NOT NULL, ADD baselinker_update_time INT DEFAULT 0 NOT NULL');
         $this->addSql('INSERT INTO baselinker_settings(name, value) VALUES("order.source", "0")');
         $this->addSql('INSERT INTO baselinker_settings(name, value) VALUES("last.journal.id", "0")');
-        $this->addSql('INSERT INTO baselinker_statuses_associations(shopStatus, baselinkerStatus) VALUES("new", "")');
-        $this->addSql('INSERT INTO baselinker_statuses_associations(shopStatus, baselinkerStatus) VALUES("fulfilled", "")');
-        $this->addSql('INSERT INTO baselinker_statuses_associations(shopStatus, baselinkerStatus) VALUES("cancelled", "")');
+        $this->addSql('INSERT INTO baselinker_statuses_associations(shop_status, baselinker_status) VALUES("new", "")');
+        $this->addSql('INSERT INTO baselinker_statuses_associations(shop_status, baselinker_status) VALUES("fulfilled", "")');
+        $this->addSql('INSERT INTO baselinker_statuses_associations(shop_status, baselinker_status) VALUES("cancelled", "")');
     }
 
     public function down(Schema $schema): void
@@ -35,6 +35,6 @@ final class Version20230328103506 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE baselinker_settings');
         $this->addSql('DROP TABLE baselinker_statuses_associations');
-        $this->addSql('ALTER TABLE sylius_order DROP baselinkerId, DROP baselinkerUpdateTime');
+        $this->addSql('ALTER TABLE sylius_order DROP baselinker_id, DROP baselinker_update_time');
     }
 }
