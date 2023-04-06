8ccdb958ca9136d96649dbe37bcbc4c50fe186be
Kamil Matulewicz <kamil.matulewicz@gmail.com>
2023-04-06 16:39:30 +0200

    Logger added to orders:add command

--- src/Command/OrdersAddCommand.php
+++ src/Command/OrdersAddCommand.php
@@ -6,6 +6,7 @@ namespace SyliusBaselinkerPlugin\Command;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
+use Psr\Log\LoggerInterface;
 use SyliusBaselinkerPlugin\Entity\OrderInterface;
 use SyliusBaselinkerPlugin\Repository\OrderRepositoryInterface;
 use SyliusBaselinkerPlugin\Service\OrdersApiServiceInterface;
@@ -21,14 +22,18 @@ class OrdersAddCommand extends Command
 
     private EntityManagerInterface $entityManager;
 
+    private LoggerInterface $logger;
+
     public function __construct(
         OrderRepositoryInterface $orderRepository,
         OrdersApiServiceInterface $orderApi,
         EntityManagerInterface $entityManager,
+        LoggerInterface $logger,
     ) {
         $this->orderRepository = $orderRepository;
         $this->orderApi = $orderApi;
         $this->entityManager = $entityManager;
+        $this->logger = $logger;
 
         parent::__construct();
     }
@@ -41,9 +46,10 @@ class OrdersAddCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        /** @todo: Log */
         /** @todo: --quiet */
+        $this->logger->debug('Command baselinker:orders:add executed.');
         $orders = $this->orderRepository->findNewOrdersToAdd();
+        $this->logger->debug(sprintf('Selecting %d orders to add.', count($orders)));
         $output->writeln('Adding orders to Baselinker:');
 
         /** @var OrderInterface $order */
@@ -63,9 +69,11 @@ class OrdersAddCommand extends Command
                     $message = 'Order ' . (string) $order->getId() .
                         ' successfully exported to Baselinker on id: ' .
                         $order->getBaselinkerId();
+                    $this->logger->debug($message);
                     $output->writeln($message);
                 } else {
                     $message = 'Order ' . (string) $order->getId() . ' ' . $exception->getMessage();
+                    $this->logger->error($message);
                     $output->writeln($message);
                     $output->writeln('Aborting');
 
