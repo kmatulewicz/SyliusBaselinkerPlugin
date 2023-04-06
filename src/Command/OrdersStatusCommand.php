0edfc4a0e4226c5f208ebe1354fff73fd708dd37
Kamil Matulewicz <kamil.matulewicz@gmail.com>
2023-04-06 17:56:45 +0200

    Logger added to orders:statuses command

--- src/Command/OrdersStatusCommand.php
+++ src/Command/OrdersStatusCommand.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace SyliusBaselinkerPlugin\Command;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Psr\Log\LoggerInterface;
 use Sylius\Component\Core\Repository\OrderRepositoryInterface;
 use SyliusBaselinkerPlugin\Entity\OrderInterface;
 use SyliusBaselinkerPlugin\Entity\Settings;
@@ -24,16 +26,20 @@ class OrdersStatusCommand extends Command
 
     private OrderStatusApplierInterface $statusApplier;
 
+    private LoggerInterface $logger;
+
     public function __construct(
         OrderRepositoryInterface $orderRepository,
         OrdersApiServiceInterface $orderApi,
         EntityManagerInterface $entityManager,
         OrderStatusApplierInterface $statusApplier,
+        LoggerInterface $logger,
     ) {
         $this->orderRepository = $orderRepository;
         $this->orderApi = $orderApi;
         $this->entityManager = $entityManager;
         $this->statusApplier = $statusApplier;
+        $this->logger = $logger;
 
         parent::__construct();
     }
@@ -47,46 +53,67 @@ class OrdersStatusCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        /** @todo: Log */
         /** @todo: --quiet */
         /** @todo: Rethink consistency: payment -> status change on Baselinker -> status change in shop */
+        $this->logger->debug('Command baselinker:orders:statuses executed.');
         $output->writeln('Synchronizing statuses:');
         /** @var Settings|null $lastJournalIdSetting */
         $lastJournalIdSetting = $this->entityManager->getRepository(Settings::class)->find('last.journal.id');
         $lastJournalId = 0;
         if (is_object($lastJournalIdSetting)) {
-            $lastJournalId = (int) $lastJournalIdSetting->getValue();
+            $lastJournalId = (int) $lastJournalIdSetting->getValue() + 1;
         } else {
             $lastJournalIdSetting = new Settings('last.journal.id');
         }
 
-        $journal = $this->orderApi->getJournalList($lastJournalId, [18]);
+        $this->logger->debug(sprintf('Get Baselinker journal from %d.', $lastJournalId));
+
+        try {
+            $journal = $this->orderApi->getJournalList($lastJournalId, [18]);
+        } catch (Exception $exception) {
+            $message = 'Cannot get Baselinker journal: ' . $exception->getMessage();
+            $this->logger->error($message);
+            $output->writeln($message);
+            $output->writeln('Aborting');
+
+            return Command::FAILURE;
+        }
+        $this->logger->debug(sprintf('Journal has %d positions.', count($journal)));
 
         /** @var array<string, int> $entry */
         foreach ($journal as $entry) {
             /** @var OrderInterface|null $order */
             $order = $this->orderRepository->findOneBy(['baselinkerId' => $entry['order_id']]);
             if (!is_object($order)) {
-                continue;
-            }
-            if ($order->getBaselinkerUpdateTime() > $entry['date']) {
+                $this->logger->debug(sprintf('Baselinker order %d not found. Omitting.', $entry['order_id']));
+                $lastJournalIdSetting->setValue((string) $entry['log_id']);
+
                 continue;
             }
 
             $output->write('Order ' . (string) $order->getId() . ': ');
-
-            $this->statusApplier->apply($order, $entry['log_type'], $entry['object_id']);
-
-            $output->writeln('updated ');
-
-            $order->setBaselinkerUpdateTime(time());
-
+            $result = $this->statusApplier->apply($order, $entry['log_type'], $entry['object_id']);
+            if (true === $result) {
+                $order->setBaselinkerUpdateTime(time());
+                $this->entityManager->persist($order);
+                $this->logger->debug(sprintf('Status of order %d updated.', (int) $order->getId()));
+                $output->writeln('updated ');
+            } else {
+                $this->logger->notice(
+                    sprintf('Status of order %d cannot be changed. Omitting.', (int) $order->getId()),
+                    [
+                        'Shop order state' => $order->getState(),
+                        'Baselinker status id' => $entry['object_id'],
+                    ],
+                );
+                $output->writeln('omitted ');
+            }
             $lastJournalIdSetting->setValue((string) $entry['log_id']);
-
-            $this->entityManager->persist($lastJournalIdSetting);
-            $this->entityManager->persist($order);
-            $this->entityManager->flush();
         }
+
+        $this->entityManager->persist($lastJournalIdSetting);
+        $this->entityManager->flush();
+        $this->logger->debug('Last journal id set to: ' . $lastJournalIdSetting->getValue());
 
         $output->writeln('Done');
 
